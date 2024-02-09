156669380a410c843ef9f18a3c0f46b761d1c0b0
Richiisontheway <mineiriccardo@gmail.com>
2024-02-09 13:59:01 +0100

    fix

--- frontend/script.js
+++ frontend/script.js
@@ -1,25 +1,32 @@
-// faccio il destructuring dell'oggetto createApp
-const { createApp } = Vue;
+// // faccio il destructuring dell'oggetto createApp
+// const { createApp } = Vue;
 
-// creazione dell'istanza di vue
+// // creazione dell'istanza di vue
 
-createApp ({
-    data(){
-        return {
-            tasks: [],
-        }
-    },
-    methods: {
-        call_api(){
-            axios
-                .get ('http://localhost/backend%20Boolean/php-todo-list-json/backend/task.php')
-                .then((response) => {
-                    console.log(response);
-                    this.tasks = response.data;
-                })
-            }
-        },
-        mounted(){
-            this.call_api();
-        }
-}).mount('#app');
+// createApp ({
+//     data(){
+//         return {
+//             tasks: [],
+//             //new_task: []
+//         }
+//     },
+//     methods: {
+//         call_api(){
+//             axios
+//                 .get ('http://localhost/backend%20Boolean/php-todo-list-json/backend/task.php')
+//                 .then((response) => {
+//                     console.log(response);
+//                     this.tasks = response.data;
+//                 })
+//             },
+//             newc(){
+//                 fast = 'ciao'
+//                 console.log(fast,typeof fast)
+//                 //console.log(this.new_task)
+//             },
+//         },
+//         mounted(){
+//             // this.call_api();
+//             // this.newc()
+//         },
+// }).mount('#app');
